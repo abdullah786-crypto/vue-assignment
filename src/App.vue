e119f08caba3dc8b35a063289314256976faf4a3
abdullah786-crypto <abdullahranjha0308@gmail.com>
2025-05-01 09:25:35 +0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+import MainFile from './Components/mainFile.vue'
+
+</script>
+
+<template>
+
+<MainFile />
+
+</template>
+
+<style>
+
+</style>
---
+++ src/Components/mainFile.vue
@@ -0,0 +1,20 @@
+<script setup>
+import BlogPost from './BlogPost/blogPost.vue';
+import './style.css'
+
+</script>
+
+<template>
+
+    <div v-bind:class="'mainDiv'">
+        <h1 v-bind:class="'mainHeading'">The Food Ninja Blog App</h1>
+        <p v-bind:class="'subHeader'">A blog about food, experiences, and recipes</p>
+        <input v-bind:class="'searchField'" placeholder="Search for acticles" />
+        <BlogPost />
+    </div>
+
+
+</template>
+<style>
+
+</style>
